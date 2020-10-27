5912fc22e2ff3b1faf967d2a72361f136f929d4d
Chaithra Pabbathi <chaithrap@gmail.com>
2020-10-27 14:00:38 -0500

    Initial Commit

---
+++ CountryHolidays/CountryHolidays/CountryHolidaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryHolidaysApp.swift
+//  CountryHolidays
+//
+//  Created by Chaithra Pabbathi on 10/27/20.
+//
+
+import SwiftUI
+
+@main
+struct CountryHolidaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This repository is used for all projects related to RESTful APIs.
